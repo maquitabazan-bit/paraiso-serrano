7e31b83b7cdc386efd8db22d8de150dc649b2e13
maquitabazan-bit <maquitabazan@gmail.com>
2026-04-19 19:22:06 -0300

    Add files via upload

---
+++ paraiso-serrano1/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", () => {
+
+	const navbar = document.getElementById("navbar");
+
+	if (window.scrollY > 80) {
+		navbar.classList.add("scrolled");
+	} else {
+		navbar.classList.remove("scrolled");
+	}
+
+});
